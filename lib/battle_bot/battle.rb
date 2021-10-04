09b943a8b41adb48af55a0178c39a31176b295df
Louis Savoie <thelousavoie@gmail.com>
2021-10-03 21:03:08 -0400

    add attributes to battle class with tests

--- lib/battle_bot/battle.rb
+++ lib/battle_bot/battle.rb
@@ -3,10 +3,12 @@
 module BattleBot
   # Battle class for storing current battle players
   class Battle
-    attr_reader :player1
+    attr_reader :player1, :player2, :log
 
-    def initialize(player1)
+    def initialize(player1, player2, log)
       @player1 = player1
+      @player2 = player2
+      @log = log
     end
   end
 end
--- spec/battle_bot/battle_spec.rb
+++ spec/battle_bot/battle_spec.rb
@@ -1,10 +1,25 @@
 # frozen_string_literal: true
 
 require 'battle_bot/battle'
+require 'battle_bot/player'
 
 RSpec.describe BattleBot::Battle do
   it 'has a player1' do
-    battle = described_class.new('test')
-    expect(battle.player1).to eq('test')
+    player1 = BattleBot::Player.new(1, 10, 1, 1)
+    player2 = BattleBot::Player.new(1, 10, 2, 2)
+    battle = described_class.new(player1, player2, "log")
+    expect(battle.player1).to eq(player1)
+  end
+  it 'has a player2' do
+    player1 = BattleBot::Player.new(1, 10, 1, 1)
+    player2 = BattleBot::Player.new(1, 10, 2, 2)
+    battle = described_class.new(player1, player2, "log")
+    expect(battle.player2).to eq(player2)
+  end
+  it 'has a log' do
+    player1 = BattleBot::Player.new(1, 10, 1, 1)
+    player2 = BattleBot::Player.new(1, 10, 2, 2)
+    battle = described_class.new(player1, player2, "log")
+    expect(battle.log).to eq("log")
   end
 end
