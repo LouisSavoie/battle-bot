ac962f54ecf4c6e974d7149641c82e0edaa496f1
Louis Savoie <thelousavoie@gmail.com>
2021-10-03 20:45:53 -0400

    add attributes to player class with tests

--- lib/battle_bot/player.rb
+++ lib/battle_bot/player.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
 module BattleBot
-  # Player class for storing players stats
+  # Player class for storing player stats
   class Player
-    attr_reader :id
+    attr_reader :id, :hp, :ap, :sp
 
-    def initialize(id)
+    def initialize(id, hp, ap, sp)
       @id = id
+      @hp = hp
+      @ap = ap
+      @sp = sp
     end
   end
 end
--- spec/battle_bot/player_spec.rb
+++ spec/battle_bot/player_spec.rb
@@ -3,8 +3,23 @@
 require 'battle_bot/player'
 
 RSpec.describe BattleBot::Player do
-  it 'has id' do
-    player = described_class.new('test')
-    expect(player.id).to eq('test')
+  it 'has an id' do
+    player = described_class.new(1, 10, 1, 1)
+    expect(player.id).to eq(1)
+  end
+
+  it 'has hp' do
+    player = described_class.new(1, 10, 1, 1)
+    expect(player.hp).to eq(10)
+  end
+  
+  it 'has ap' do
+    player = described_class.new(1, 10, 1, 1)
+    expect(player.ap).to eq(1)
+  end
+  
+  it 'has sp' do
+    player = described_class.new(1, 10, 1, 1)
+    expect(player.sp).to eq(1)
   end
 end
