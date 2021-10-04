195dbb86501fce8e6b2283340fe0c890231ca941
Louis Savoie <thelousavoie@gmail.com>
2021-10-04 14:48:01 -0400

    refactor battle and player specs with let

--- spec/battle_bot/battle_spec.rb
+++ spec/battle_bot/battle_spec.rb
@@ -4,24 +4,19 @@ require 'battle_bot/battle'
 require 'battle_bot/player'
 
 RSpec.describe BattleBot::Battle do
+  let(:player1) { BattleBot::Player.new(1, 10, 1, 1) }
+  let(:player2) { BattleBot::Player.new(1, 10, 2, 2) }
+  let(:battle) {described_class.new(player1, player2, 'log')}
+
   it 'has a player1' do
-    player1 = BattleBot::Player.new(1, 10, 1, 1)
-    player2 = BattleBot::Player.new(1, 10, 2, 2)
-    battle = described_class.new(player1, player2, 'log')
     expect(battle.player1).to eq(player1)
   end
 
   it 'has a player2' do
-    player1 = BattleBot::Player.new(1, 10, 1, 1)
-    player2 = BattleBot::Player.new(1, 10, 2, 2)
-    battle = described_class.new(player1, player2, 'log')
     expect(battle.player2).to eq(player2)
   end
 
   it 'has a log' do
-    player1 = BattleBot::Player.new(1, 10, 1, 1)
-    player2 = BattleBot::Player.new(1, 10, 2, 2)
-    battle = described_class.new(player1, player2, 'log')
     expect(battle.log).to eq('log')
   end
 end
--- spec/battle_bot/player_spec.rb
+++ spec/battle_bot/player_spec.rb
@@ -3,23 +3,23 @@
 require 'battle_bot/player'
 
 RSpec.describe BattleBot::Player do
-  it 'has a user' do
-    player = described_class.new(1, 10, 1, 1)
-    expect(player.user).to eq(1)
-  end
+  describe 'stats' do
+    let(:player) { described_class.new(1, 10, 1, 1) }
 
-  it 'has health' do
-    player = described_class.new(1, 10, 1, 1)
-    expect(player.health).to eq(10)
-  end
+    it 'has a user' do
+      expect(player.user).to eq(1)
+    end
 
-  it 'has damage' do
-    player = described_class.new(1, 10, 1, 1)
-    expect(player.damage).to eq(1)
-  end
+    it 'has health' do
+      expect(player.health).to eq(10)
+    end
 
-  it 'has speed' do
-    player = described_class.new(1, 10, 1, 1)
-    expect(player.speed).to eq(1)
+    it 'has damage' do
+      expect(player.damage).to eq(1)
+    end
+
+    it 'has speed' do
+      expect(player.speed).to eq(1)
+    end
   end
 end
