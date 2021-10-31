341bee218fb3447ba359de05047d3fd857467655
Louis Savoie <thelousavoie@gmail.com>
2021-10-31 19:26:14 -0400

    add Database.read_file and test

--- lib/battle_bot/database.rb
+++ lib/battle_bot/database.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'yaml'
+
 module BattleBot
   # Database class for storing all current battle data.
   class Database
@@ -10,7 +12,16 @@ module BattleBot
     end
 
     def new_file(data_file = 'data.yml')
-      File.open(data_file, 'w') { |file| file.write('') }
+      File.open(data_file, 'w') { |file| file.write(@data) }
+    end
+
+    def read_file(data_file = 'data.yml')
+      data_from_file = YAML.safe_load(File.read(data_file), [Symbol])
+    rescue Errno::ENOENT
+      new_file(data_file)
+      @data = YAML.safe_load(File.read(data_file))
+    else
+      @data = data_from_file
     end
   end
 end
--- spec/battle_bot/database_spec.rb
+++ spec/battle_bot/database_spec.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
 require 'battle_bot/database'
+require 'yaml'
 
 RSpec.describe BattleBot::Database do
   let(:database) { described_class.new }
+
+  testdata = { server1: { battle1: { player1: 'Tom', player2: 'Jake' },
+                          battle2: { player1: 'Carol', player2: 'Jackie' } },
+               server2: { battle1: { player1: 'DQ', player2: 'Dave' },
+                          battle2: { player1: 'Joe', player2: 'Tyler' } } }
 
   it 'has a data hash' do
     expect(database.data.class).to eq(Hash)
@@ -12,8 +18,17 @@ RSpec.describe BattleBot::Database do
   describe '.new_file' do
     it 'creates a blank yaml file' do
       database.new_file('test.yml')
-      expect(File.read('test.yml')).to eq('')
+      expect(YAML.safe_load(File.read('test.yml'))).to eq(database.data)
       File.delete('test.yml')
+    end
+  end
+
+  describe '.read_file' do
+    it 'reads YAML file into @data' do
+      File.open('readtest.yml', 'w') { |file| file.write(testdata.to_yaml) }
+      database.read_file('readtest.yml')
+      expect(database.data).to eq(testdata)
+      File.delete('readtest.yml')
     end
   end
 end
