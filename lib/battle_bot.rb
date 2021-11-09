93f74af80d4b7ecbef17906f5d0dd9e79bf484d2
Louis Savoie <thelousavoie@gmail.com>
2021-11-09 16:18:47 -0500

    change to CommandBot, add help, challenge, and accept command boilerplate

--- lib/battle_bot.rb
+++ lib/battle_bot.rb
@@ -12,10 +12,31 @@ module BattleBot
   end
 
   if ENV['BOT_TOKEN']
-    self.bot = Discordrb::Bot.new token: ENV['BOT_TOKEN']
+    self.bot = Discordrb::Commands::CommandBot.new token: ENV['BOT_TOKEN'], prefix: 'bb '
+    bot.ready do
+      bot.watching = 'bb help'
+    end
 
-    bot.message(with_text: 'Ping!') do |event|
-      event.respond 'Pong!'
+    bot.command :help do |event|
+      event.respond 'I\'m helpful!'
+    end
+
+    bot.command :challenge do |event|
+      if event.message.mentions.empty?
+        event.respond 'Who are you challenging?'
+      else
+        challenged = event.message.mentions[0].mention
+        event.respond "#{event.author.mention} challenges #{challenged}!"
+      end
+    end
+
+    bot.command :accept do |event|
+      if event.message.mentions.empty?
+        event.respond 'Who\'s challenge are you accepting?'
+      else
+        challenger = event.message.mentions[0].mention
+        event.respond "#{event.author.mention} accepts #{challenger}'s challenge!"
+      end
     end
   end
 end
