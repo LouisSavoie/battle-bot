e384848a415d4ef07644b2f71f6c70fc65517ddb
Louis Savoie <thelousavoie@gmail.com>
2021-12-09 18:42:21 -0500

    char command channel specific, uses mentions, creates server and player

--- lib/battle_bot.rb
+++ lib/battle_bot.rb
@@ -91,23 +91,38 @@ module BattleBot
     end
 
     bot.command :char do |event|
-      # create the server if it doesn't exist in db
-      db.add_server BattleBot::Server.new event.server.id
-      if db.data[event.server.id].players[event.author.id]
-        char = db.data[event.server.id].players[event.author.id]
-      else
-        # create players if they don't exist in db>server
-        char = check_for_player event.server.id, event.author.id, event.author.name
-        db.add_player event.server.id, char
+      # ignore messages not in the bb-arena channel
+      if event.channel.name == 'bb-arena'
+        # create the server if it doesn't exist in db
+        db.add_server BattleBot::Server.new event.server.id
+        if event.message.mentions[0]
+          player = event.message.mentions[0]
+          if db.data[event.server.id].players[event.message.mentions[0].id]
+            char = db.data[event.server.id].players[event.message.mentions[0].id]
+          else
+            # create player if they don't exist in db>server
+            char = check_for_player event.server.id, event.message.mentions[0].id, event.message.mentions[0].name
+            db.add_player event.server.id, char
+          end
+        else
+          player = event.author
+          if db.data[event.server.id].players[event.author.id]
+            char = db.data[event.server.id].players[event.author.id]
+          else
+            # create player if they don't exist in db>server
+            char = check_for_player event.server.id, event.author.id, event.author.name
+            db.add_player event.server.id, char
+          end
+        end
+        event.respond <<~INFO
+          #{player.mention}'s Character Info:
+          __**#{char.name}**__
+          Health: **#{char.max_health}**
+          Damage: **#{char.damage}**
+          Speed: **#{char.speed}**
+          Hit: **#{char.hit}**
+        INFO
       end
-      event.respond <<~INFO
-        #{event.author.mention}'s Character Info:
-        __**#{char.name}**__
-        Health: **#{char.max_health}**
-        Damage: **#{char.damage}**
-        Speed: **#{char.speed}**
-        Hit: **#{char.hit}**
-      INFO
     end
   end
 end
